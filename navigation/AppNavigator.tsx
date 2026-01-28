5d02021cf1640754f96708521f48d459ad464ab8
Panos9393 <panos93-7@hotmail.com>
2026-01-28 22:03:58 +0200

    Language additions

--- navigation/AppNavigator.tsx
+++ navigation/AppNavigator.tsx
@@ -1,3 +1,4 @@
+console.log("🔥 USING THIS AppNavigator FILE");
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React, { useEffect } from "react";
