720d6546849706e4415ba5b022b812dbb0a07422
Panos9393 <panos93-7@hotmail.com>
2026-02-02 22:32:45 +0200

    Language additions

--- screens/SmartScanScreen.tsx
+++ screens/SmartScanScreen.tsx
@@ -491,7 +491,7 @@ export default function SmartScanScreen({ navigation }: any) {
   activeOpacity={0.9}
   style={{
     width: "100%",
-    backgroundColor: "#2E3FEF",   // ⭐ metallic blue
+    backgroundColor: "#1E2A45",   // ⭐ metallic blue
     paddingVertical: 18,
     borderRadius: 14,
     alignItems: "center",
